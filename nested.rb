e51e47bf128b9dcac9b6ef5b845e7a68ca1844ae
David Kirsch <davidjkirsch@gmail.com>
2020-06-24 17:52:59 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,8 +84,10 @@ def adding_matz
       }
     }
   programmer_hash[:yukihiro_matsumoto] = {}
-  programmer_hash[:yukihiro_matsumoto][:known_for] = 
-  programmer_hash[:yukihiro_matsumoto][:languages] = 
+  programmer_hash[:yukihiro_matsumoto][:known_for] = nil
+  programmer_hash[:yukihiro_matsumoto][:languages] = nil
+  programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+  programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
   programmer_hash
 end
 
